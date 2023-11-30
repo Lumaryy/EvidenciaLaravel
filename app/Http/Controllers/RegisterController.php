a38de69da59a8c5a2aa48da7afe8d7ffddbca1e3
daniel22alejandro <danniel10yara@gmail.com>
2023-11-29 23:34:14 -0500

    gaspacho

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,4 +9,30 @@ class RegisterController extends Controller
     public function index(){
             return view ('principal');
     }
+
+    public function store(Request $request){
+        $request->request->add([ 'cedula'=> Str::slug($request->cedula)]);
+
+
+        $this->validate($request,[ 
+
+            'nombre'=>'required|min:3|max:50',
+            'cedula'=>'required|unique:users|min:3|max:50',
+            'telefono'=>'required|unique:users|max:60',
+            'cedula'=>'required|confirmed'
+        ]);
+
+        users::create([ 
+            'nombre'=>$request->nombre,
+            'cedula'=>$request->cedula,
+            'telefono'=>$request->telefono,
+            'direccion'=>$request->direccion,
+        ]);
+        auth()->attemp([ 
+            'nombre'=>$request->nombre,
+            'cedula'=>$request->cedula,
+        ]);
+
+        return redirect()->route('post.index');
+    }
 }
